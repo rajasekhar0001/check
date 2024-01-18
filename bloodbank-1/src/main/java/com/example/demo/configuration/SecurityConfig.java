43639ba89c85c147ea29efd2dbc78c51cce7c287
Yash Rai <yash_rai@thbs.india.com>
2024-01-18 10:47:24 +0530

    yash

--- bloodbank-1/src/main/java/com/example/demo/configuration/SecurityConfig.java
+++ bloodbank-1/src/main/java/com/example/demo/configuration/SecurityConfig.java
@@ -69,6 +69,7 @@ public class SecurityConfig {
 	        .authorizeHttpRequests(request -> request
 	        	.requestMatchers("/admin/static/css/**", "/admin/static/plugins/**", "/admin/static/js/**", "/admin/static/images/**",  "/admin/static/webjars/**").permitAll()
 	        	.requestMatchers("/user/static/**", "/user/static/js/**").permitAll()
+	        	.requestMatchers("/static/css/assets/**","static/css/vendors/**").permitAll()
 	        	.requestMatchers("/adminHome").hasAuthority("ADMIN")
 	            .requestMatchers("/userHome").hasAuthority("USER")
 	            .requestMatchers("/", "/userLogin", "/logout", "/register", "/sendOTP/{email}").permitAll()  // Allow public access for login and logout
