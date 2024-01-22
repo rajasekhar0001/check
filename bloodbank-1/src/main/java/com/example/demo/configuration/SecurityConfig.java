2e37b0bbf22801dc9f2ec64b1a867181e3a50b83
rajsekhar0001 <bodeddularajasekharreddy1111@gmail.com>
2024-01-22 09:18:38 +0530

    user

--- bloodbank-1/src/main/java/com/example/demo/configuration/SecurityConfig.java
+++ bloodbank-1/src/main/java/com/example/demo/configuration/SecurityConfig.java
@@ -3,14 +3,19 @@ package com.example.demo.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
@@ -57,7 +62,10 @@ public class SecurityConfig {
 	    return http.build();
 	}
 
-
+	
+	  
+	
+	
 	@Autowired
 	public void configure (AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
